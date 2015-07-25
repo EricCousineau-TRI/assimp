f9eecaf2638fc6176df56a773f5d3e031da97c28
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-07-25 21:07:47 +0200

    helper-script for importing sources

---
+++ debian/import_upstream.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+UPSTREAM=$1
+
+if [ -e "${UPSTREAM}" ]; then
+ git-import-orig \
+  --filter .gitignore \
+  --filter test/ \
+  --filter-pristine-tar --pristine-tar \
+  --sign-tags \
+  ${UPSTREAM}
+else
+  echo "usage: $0 <upstream.tgz>" 1>&2
+  exit 1
+fi
